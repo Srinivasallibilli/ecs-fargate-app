21ef4ef25d171ba1bb32011f76a53ee01f826b53
I'm-srinivas <130681332+Srinivasallibilli@users.noreply.github.com>
2025-11-06 13:04:11 +0530

    Modify home route response message
    Updated the home route response to include project details and environment.

--- app/server.py
+++ app/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hello from ECS Fargate! Environment: {os.getenv('ENV', 'dev')}"
+    return f"Hello this srinivas allibilli Successfully implemented the project by setting up a CI/CD pipeline, containerizing the application using Docker, deploying it on AWS (ECS/Fargate), and automating infrastructure with Terraform — achieving faster release cycles and improved reliability.”from ECS Fargate! Environment: {os.getenv('ENV', 'DEV','TEST')}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
