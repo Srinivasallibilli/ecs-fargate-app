532e3f4a60010f99d25067572b69655bcfc323ce
I'm-srinivas <130681332+Srinivasallibilli@users.noreply.github.com>
2025-11-06 15:03:55 +0530

    Update home route message with project details

--- app/server.py
+++ app/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hello from ECS Fargate! Environment: {os.getenv('ENV', 'dev')}"
+    return f"Hello Successfully implemented the project by setting up a CI/CD pipeline, containerizing the application using Docker, deploying it on AWS (ECS/Fargate) from ECS Fargate! Environment: {os.getenv('ENV', 'dev')}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
